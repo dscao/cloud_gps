eedf63beb2285ce9628ae44315ff096fa3bb2c4d
dscao <dscao.com@gmail.com>
2025-01-05 23:16:26 +0800

    Update __init__.py
    "Config" is deprecated and will be removed in HA Core 2025.11

--- custom_components/cloud_gps/__init__.py
+++ custom_components/cloud_gps/__init__.py
@@ -38,7 +38,8 @@ from homeassistant.components.device_tracker import PLATFORM_SCHEMA
 from homeassistant.components.device_tracker.const import CONF_SCAN_INTERVAL
 from homeassistant.components.device_tracker.legacy import DeviceScanner
 
-from homeassistant.core import Config, HomeAssistant
+from homeassistant.core import HomeAssistant
+from homeassistant.core_config import Config
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
